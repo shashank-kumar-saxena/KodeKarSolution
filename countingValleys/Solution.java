e8338b5019c3d1e8ddb7ed20b15ac8167868a70b
Shashank <shashanksaxena226@gmail.com>
2022-04-18 11:56:00 +0530

    CountingValleys

---
+++ countingValleys/Solution.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+
+    public static int countingValleys(int steps, String path) {
+        int count=0,fCount=0;
+        for(int i=0;i<steps;i++)
+        {
+            char c=path.charAt(i);
+            if(c=='D')
+                count--;
+            else if(c=='U')
+            {count++;
+                if(count==0)
+                    fCount++;}
+        }
+        return fCount;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
